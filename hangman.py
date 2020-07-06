dfeba6a9d61747ea19be2b18d7d2ac8a0a917c66
Simon <67862348+simon-stevens802@users.noreply.github.com>
2020-07-06 21:10:52 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,21 +4,22 @@ def hangman():
     import re
 
 #open the wordlist
-    with open('words.txt','r')as words:
+    file = 'word_list.txt'
+    with open(file,'r')as words:
         word_list = words.read().splitlines()
 
-#set number of lives
-    lives = 10
+#set number of lives to 7
+    lives = 7
 
 #choose a random word for the game
     random_num = random.randint(0, len(word_list)-1)
     chosen_word = word_list[random_num]
 
 #hide the chosen_word
-    secret_word = re.sub('[0-9a-zA-Z]','-',chosen_word)
+    secret_word = re.sub('[0-9a-zA-Z]','*',chosen_word)
 
 
-###define process to andle player guesses
+###define process to handle player guesses
 # define 'guess' as the function, with 'letter', 'word' and 'encoded' as strings
     def guess(letter, word, encoded):
 
@@ -41,15 +42,15 @@ def hangman():
 
 #start the game
     print("Time to play, you have", lives, "lives.")
-    print("Your word is:", secret_word)
+    print("The word to guess is:", secret_word)
     print("Lets play!")
     
-    #set a while loop to run for 10 lives
+# set a while loop to run for 7 lives
     while(lives > 0):
 # get the guessed letter from player, assign that letter to variable 'guessed'
-        guessed = input("What's your guess: ")[:1]       
+        guessed = input("Please enter your next guess: ")[:1]       
 # assign a correct guess and the secret_word to the pre-defined 'guess' function
-    letter_found, secret_word = guess(guessed, chosen_word, secret_word)
+        letter_found, secret_word = guess(guessed, chosen_word, secret_word)
 
 # check if the guessed letter exists in the hidden word
         if not letter_found:
@@ -57,36 +58,25 @@ def hangman():
             lives -= 1
 # check if there are no more lives left - if none, end game. 
             if lives == 0:
-                print("Oh no! You've used up your lives. The word was:",chosen_word,". Better luck next time!")
+                print("I'm sorry, you have no lives left. The word was:", chosen_word)
+                print("You lose.")
+ 
                 break
 # check lives left, if yes tell player wrong guess and show lives left, move to next guess, show word left to guess.                
             else:
-                print("Ooops, that letter isn't in the word, you have",lives,"lives remaining.")
-                print(secret_word)
-
+                print("Ooops, that letter isn't in the word, you have",lives,"lives remaining. The word to guess is: ", secret_word)
+                
 # if the guessed letter isn't in the word
 # first check if the word still has characters to guess
         else:
-            if "#" not in secret_word:
-# no letters left to guess - player wins                
-                print("Congratulations! You won with", lives,"remaining. The word was", chosen_word,".")
+            if "*" not in secret_word:
+# no letters left to guess - player wins
+                print("Well done, the word was:", chosen_word)
+                print("Congratulations you win")
+
                 break
 # correct guess, more letters left to guess. Show lives left, show word left to guess.     
             else:
-                print("well done! That letter was found. You still have", lives, "remaining.")
-                print(secret_word)
-                
-# see if the player wants to play again? reset the game
-    restart = input("Want to play again? (y = yes, n = no) ")
-# player wants to play again, they enter 'y' and press enter
-    if restart == "y":
-        hangman()
-        
-# player doesn't want to play again, game ends.
-    else:
-        print("Ok, thanks for playing")
-        print("        \|/        ") 
-        print("_____mm_0,0_mm_____")
-        
+                print("well done! That letter was found. You still have", lives, "remaining. The word to guess is: ", secret_word)                
 # begin the game
 hangman()
